603e92af52a9cf5dabf9389165d2c9699ed5b3b7
Pip Oomen <pepijn@redpill-linpro.com>
2016-12-19 15:19:06 +0100

    adjust for openshift 3

--- wsgi.py
+++ wsgi.py
@@ -4,25 +4,21 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # Foobar is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
+from flask import Flask
+application = Flask(__name__)
 
-virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+@application.route('/')
+def hello_world():
+    return "Hello Python World!\r\n", 200, { 'Content-Type': 'text/plain' }
 
-from hello_world import app as application
-
-#
-# Below for testing only
-#
 if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
-    make_server('localhost', 5000, application).serve_forever()
+    application.run(debug = True)
